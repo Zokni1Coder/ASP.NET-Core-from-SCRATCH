537e00915b4a7b60081a3429a2fabe49dad825b3
Zokni1Coder <erikkovacs0000@gmail.com>
2026-02-24 18:24:00 +0100

    Model Validations - DataAnnotations

---
+++ ModelValidationExample/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationExample.Models
+{
+    public class Person
+    {
+        [Required]
+        public string? PersonName { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Phone]
+        public string? Phone { get; set; }
+        [MinLength(8)]
+        public string? Password { get; set; }
+        [Compare("Password")]
+        public string? ConfirmPassword { get; set; }
+        [Range(0, int.MaxValue)]
+        public double? Price { get; set; }
+        public override string ToString()
+        {
+            return $"Person object - Name: {PersonName}, Email: {Email}, Phone: {Phone}, Password: {Password}, ConfirmPassword: {ConfirmPassword}, Price: {Price}";
+        }
+    }
+}
