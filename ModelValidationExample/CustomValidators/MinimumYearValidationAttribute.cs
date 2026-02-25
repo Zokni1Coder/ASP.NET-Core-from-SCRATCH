b6bc899a7a11425d0227eaf7f71c0383b58339b2
Zokni1Coder <erikkovacs0000@gmail.com>
2026-02-25 23:16:07 +0100

    Custom Validation

---
+++ ModelValidationExample/CustomValidators/MinimumYearValidationAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Diagnostics;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationExample.CustomValidations
+{
+    public class MinimumYearValidationAttribute : ValidationAttribute 
+    {
+        public int MinimumYear { get; set; } = 2000;
+        // Opcionális paraméter nélküli konstruktor.
+        // Akkor szükséges, ha az attribútumot paraméter nélkül szeretnénk használni.
+        public MinimumYearValidationAttribute(){}
+        // Konstruktor a minimum évszám beállításához.
+        // Így használható: [MinimumYearValidation(2005)]
+        public MinimumYearValidationAttribute(int minimumYear)
+        {
+           this.MinimumYear = minimumYear; 
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+                return null;
+
+            DateTime date = (DateTime)value;
+            if (date.Year >= this.MinimumYear)
+            {
+                //return new ValidationResult($"The year should be less than {this.MinimumYear}.");
+
+                //Ha a Person osztályban az attribútumnál megadsz egy error üzenetet, akkor az alábbi két példával tudod megjeleníteni. Máskülönben az első változat használatos.
+                //return new ValidationResult($"{ErrorMessage} {this.MinimumYear}.");
+                return new ValidationResult(string.Format(ErrorMessage, this.MinimumYear));
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
--- ModelValidationExample/Models/Person.cs
+++ ModelValidationExample/Models/Person.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using ModelValidationExample.CustomValidations;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
@@ -25,11 +26,15 @@ namespace ModelValidationExample.Models
         [DisplayName("Confirm Password")]
         public string? ConfirmPassword { get; set; }
         
-        [Range(0, 999.99, ErrorMessage = "The Price must be between {1} and {2}.")] //Ebben az esetben az {1} az első paraméter értékre, a {2} a másodikra mutat. a {0}, az minden esetben maga a property neve amit megszorítunk (ha DisplayName annotáció van rajta, akkor azt fogja megejeleníteni). 
+        [Range(0, 999.99, ErrorMessage = "The Price must be between {1} and {2}.")] //Ebben az esetben az {1} az első paraméter értékre, a {2} a másodikra mutat. a {0}, az minden esetben maga a property neve amit megszorítunk (ha DisplayName annotáció van rajta, akkor azt fogja megejeleníteni). Ezeket a számokat nevezzük Placeholderoknak.
         public double? Price { get; set; }
+
+        //[MinimumYearValidation]
+        [MinimumYearValidation(2005, ErrorMessage = "The year should be less than {0}.")]
+        public DateTime? DateOfBirth { get; set; } 
         public override string ToString()
         {
-            return $"Person object - Name: {PersonName}, Email: {Email}, Phone: {Phone}, Password: {Password}, ConfirmPassword: {ConfirmPassword}, Price: {Price}";
+            return $"Person object - Name: {PersonName}, Email: {Email}, Phone: {Phone}, Password: {Password}, ConfirmPassword: {ConfirmPassword}, Price: {Price}, Date of Birth: {DateOfBirth}";
         }
     }
 }
