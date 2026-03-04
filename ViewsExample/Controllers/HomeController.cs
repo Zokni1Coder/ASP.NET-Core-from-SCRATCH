43446ff32c1272478e9ca90b18a28fd38f3410a9
Zokni1Coder <erikkovacs0000@gmail.com>
2026-03-04 19:25:25 +0100

    ViewData - Part 1

--- ViewsExample/Controllers/HomeController.cs
+++ ViewsExample/Controllers/HomeController.cs
@@ -14,7 +14,20 @@ namespace ViewsExample.Controllers
             //1.) Controllerekre bontva, hozz létre mappákat. Esetünkben ez a /View/Home mappa.
             //2.) Minden View, ami az adott Controllerhez tartozik, az annak megfelelő almappába mentsd. Esetünkben minden View, amit a Home Controller meg fog jeleníteni valamikor, a Views/Home mappába kerül.
 
-            
+
+            //Mint ahogy láthatod, minden definiálási rész átkerült ide.
+            ViewData["title"] = "Home - From c# block";
+            Person person = new Person()
+            {
+                DateOfBirth = Convert.ToDateTime("2005-05-18"),
+                Name = "Réka",
+                Gender = Gender.female,
+                FavoriteCities = new List<string> { "Szabadka", "Salzburg", "Eger", "Rimini" }
+            };
+            ViewData["person"] = person;
+
+            ViewData["alertMessage"] = $"<script>alert('{person.Name}-nak/nek {person.FavoriteCities.Count} kedvenc városa van!')</script>";
+
             return View(); //Ha nem adunk meg paraméterül View-t, akkor mindig ezt a sablont követi: "/View/Controller-név/Controller-metódusnév.cshtml". Tehát esetünkben nem kell most semmit sem kihangsúlyozni, mert a View útvonala és neve: "/Views/Home/Index.cshtml"
 
             //return View(ABC); => /View/Home/ABC.cshtml
