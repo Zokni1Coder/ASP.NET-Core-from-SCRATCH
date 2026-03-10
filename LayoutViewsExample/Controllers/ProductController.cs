f5f2ed8e944f35d9286c0f92e1a2d75279df30da
Zokni1Coder <erikkovacs0000@gmail.com>
2026-03-10 14:56:15 +0100

    LayoutView, Controllers, CSS.

---
+++ LayoutViewsExample/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewsExample.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("products")]
+        public IActionResult Product()
+        {
+            return View();
+        }
+        [Route("search")]
+        public IActionResult Search()
+        {
+            return View();
+        }
+        [Route("order")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
