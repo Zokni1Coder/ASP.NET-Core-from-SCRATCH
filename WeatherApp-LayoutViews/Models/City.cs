d4a076763d2ea6a4d2938221a0ceaa39b70234b1
Zokni1Coder <erikkovacs0000@gmail.com>
2026-03-11 19:16:58 +0100

    Solution for WeatherApp with Layout Views

---
+++ WeatherApp-LayoutViews/Models/City.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherApp_LayoutViews.Models
+{
+    public class City
+    {
+        public string CityUniqueCode { get; set; }
+        public string CityName { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int TemperatureFahrenheit { get; set; }
+    }
+}
