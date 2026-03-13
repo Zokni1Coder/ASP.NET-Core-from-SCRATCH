e8f1fdc99f16fba1842da26f6c5f984a948eba7f
Zokni1Coder <erikkovacs0000@gmail.com>
2026-03-13 22:42:10 +0100

    Partial Views with ViewData

--- PartialllViewsExample/Controllers/HomeController.cs
+++ PartialllViewsExample/Controllers/HomeController.cs
@@ -7,11 +7,14 @@ namespace PartialllViewsExample.Controllers
         [Route("/")]
         public IActionResult Index()
         {
+            ViewData["ListTitle"] = "Cities";
+            ViewData["ListItems"] = new List<string> { "Szabadka", "Kanizsa", "Kelebia", "Horgos", "Palics", "Hajdujárás" };
             return View();
         }
         [Route("/about")]
         public IActionResult About()
         {
+            ViewData["ListTitle"] = "About";
             return View();
         }
     }
