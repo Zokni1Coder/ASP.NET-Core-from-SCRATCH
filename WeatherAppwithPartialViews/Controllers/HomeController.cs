bb94f4e2d7062f95c4d836fdabb37f846c895b1b
Zokni1Coder <erikkovacs0000@gmail.com>
2026-03-14 23:24:54 +0100

    Solution for WeatherApp with Partial Views

---
+++ WeatherAppwithPartialViews/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAppwithPartialViews.Models;
+
+namespace WeatherAppwithPartialViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<City> cityList = new List<City>()
+            {
+                new City()
+                {
+                    CityUniqueCode = "LND",
+                    CityName = "London",
+                    DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),
+                    TemperatureFahrenheit = 33
+                },
+                new City()
+                {
+                    CityUniqueCode = "NY",
+                    CityName = "New York",
+                    DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),
+                    TemperatureFahrenheit = 60
+                },
+                new City()
+                {
+                    CityUniqueCode = "PAR",
+                    CityName = "Paris",
+                    DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),
+                    TemperatureFahrenheit = 82
+                }
+            };
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(cityList);
+        }
+
+        [Route("/weather/{cityCode}")]
+        public IActionResult Select(string cityCode)
+        {
+            City? temp = cityList.Where(code => code.CityUniqueCode == cityCode).FirstOrDefault();
+
+            return View(temp);
+        }
+    }
+}
