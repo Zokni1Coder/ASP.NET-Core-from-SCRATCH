4817bdcd091e28810499e7dd1e6c9b5607df0e15
Zokni1Coder <erikkovacs0000@gmail.com>
2026-03-16 19:10:03 +0100

    Solution for WeatherApp with View Component

---
+++ WeatherAppWithViewComponents/Components/WeatherViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAppWithViewComponents.Models;
+
+namespace WeatherAppWithViewComponents.Components
+{
+    public class WeatherViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(City city)
+        {
+            return View("cityWeather", city);
+        }
+    }
+}
