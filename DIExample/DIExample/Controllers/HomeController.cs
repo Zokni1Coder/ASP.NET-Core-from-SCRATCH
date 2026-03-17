5fe47ba669ef346617c055f07684ee4b71420363
Zokni1Coder <erikkovacs0000@gmail.com>
2026-03-17 15:37:18 +0100

    Services

---
+++ DIExample/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Létrehozol egy readonly mezőt a Service-ből.
+        private readonly CitiesService _citiesService;
+        public HomeController()
+        {
+            //A mezőnek létrehozol egy egyedet. Ez nem DI, de ezzel majd később foglalkozunk.
+            this._citiesService = new CitiesService();
+        }
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //Így el tudjuk érni a Service metódusát és értéket fogunk kapni.
+            List<string> cities = _citiesService.GetCities;
+            return View(cities);
+        }
+    }
+}
---
+++ DIExample/Services/CitiesService.cs
@@ -0,0 +1,33 @@
+﻿namespace Services
+{
+    //Amikor egy Service-szel működő programot hozol létre, akkor mindig a 
+    //Service résszel illik kezdeni.
+    //Ez a szervice ezt kb úgy kell elképzelni mint mescben mikor tanultuk a 
+    //.dll-eket létrehozni és használni. Mivel egy solutionben lesz két projektünk,
+    //meg kell adni a Service referencáját a DIExample-nek. Fontos az irány!
+    //Referencia megadása: DIExample/Dependencies->Add Project Reference -> 
+    //Kipipálod a Services-t és így el fogod tudni érni a metódusokat a másik projektből is.
+    public class CitiesService
+    {
+        private List<string> _cities { get; set; }
+
+        public List<string> GetCities
+        {
+            get
+            {
+                return _cities;
+            }
+        }
+        public CitiesService()
+        {
+            _cities = new List<string>()
+             {
+                 "London",
+                 "Paris",
+                 "New York",
+                 "Tokyo",
+                 "Rome"
+             };
+        }
+    }
+}
