e7f2213c16274b3246735fc80480fa51eb238d43
Zokni1Coder <erikkovacs0000@gmail.com>
2026-03-19 14:57:11 +0100

    Inversion of Control

--- DIExample/DIExample/Program.cs
+++ DIExample/DIExample/Program.cs
@@ -1,5 +1,15 @@
+using ServiceContracts;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+//Itt adjuk hozzá az IoC konténerhez a Service-ünket.
+builder.Services.Add(new ServiceDescriptor(
+    typeof(ICitiesService),
+    typeof(CitiesService),
+    ServiceLifetime.Transient    //Transient/Scoped/Singleton
+    ));
 
 var app = builder.Build();
 
