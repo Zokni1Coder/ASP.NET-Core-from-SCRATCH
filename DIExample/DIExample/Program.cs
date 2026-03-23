99d7e683594a966b4e31574075be1eb0a81941fa
Zokni1Coder <erikkovacs0000@gmail.com>
2026-03-23 20:39:47 +0100

    AddTransient(), AddScoped(), AddSingleton()

--- DIExample/DIExample/Program.cs
+++ DIExample/DIExample/Program.cs
@@ -5,13 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //Itt adjuk hozzá az IoC konténerhez a Service-ünket.
-builder.Services.Add(new ServiceDescriptor(
-    typeof(ICitiesService),
-    typeof(CitiesService),
-    //ServiceLifetime.Transient    //Transient/Scoped/Singleton
-    //Itt állítsd át az életciklusát Scoped-ra.
-    ServiceLifetime.Scoped
-    ));
+//builder.Services.Add(new ServiceDescriptor(
+//    typeof(ICitiesService),
+//    typeof(CitiesService),
+//    //ServiceLifetime.Transient    //Transient/Scoped/Singleton
+//    //Itt állítsd át az életciklusát Scoped-ra.
+//    ServiceLifetime.Scoped
+//    ));
+
+//A fenti kódnak, amivel a konténerhez adunk egy service-t, van rövidebb változata is:
+/*
+    builder.Services.AddTransient<ICitiesService, CitiesService>();
+    builder.Services.AddSingleton<ICitiesService, CitiesService>();
+*/
+builder.Services.AddScoped<ICitiesService, CitiesService>();
+
 
 var app = builder.Build();
 
