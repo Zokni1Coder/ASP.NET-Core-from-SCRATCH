96136725c89a43fd383d7c18eb50d098508fae88
Zokni1Coder <erikkovacs0000@gmail.com>
2026-03-25 15:55:33 +0100

    Environment in Launch Settings

---
+++ EnvironmentsExample/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("something")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //A következő endpointot ugyanarra a routra állítjuk mint az Indexet. Figyeld meg hogy ugyanazt az errort a böngészőben hogyan jeleníti meg Development, Staging és Production környezetben!
+        [Route("something")]
+        public IActionResult Other()
+        {
+            return View();
+        }
+    }
+}
