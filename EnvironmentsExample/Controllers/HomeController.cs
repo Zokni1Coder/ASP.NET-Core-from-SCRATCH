0f84ad99e48d52731247521ea6bdd6979a3b9ecf
Zokni1Coder <erikkovacs0000@gmail.com>
2026-03-25 16:16:42 +0100

    Environment in Controller

--- EnvironmentsExample/Controllers/HomeController.cs
+++ EnvironmentsExample/Controllers/HomeController.cs
@@ -4,17 +4,26 @@ namespace EnvironmentsExample.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        //Az IWebHostEnvironment-et azért nem kell regisztrálni a DI/IoC Containerbe, mert amikor az app elindul, automatikusan bele kerül egy csomó service-t és a IWebHostEnvironment is egy ilyen. 
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+             this._webHostEnvironment = webHostEnvironment;
+        }
+
         [Route("/")]
-        [Route("something")]
+        //[Route("something")]
         public IActionResult Index()
         {
+            ViewBag.Environment = this._webHostEnvironment.EnvironmentName;
             return View();
         }
         //A következő endpointot ugyanarra a routra állítjuk mint az Indexet. Figyeld meg hogy ugyanazt az errort a böngészőben hogyan jeleníti meg Development, Staging és Production környezetben!
-        [Route("something")]
-        public IActionResult Other()
-        {
-            return View();
-        }
+        //[Route("something")]
+        //public IActionResult Other()
+        //{
+        //    return View();
+        //}
     }
 }
