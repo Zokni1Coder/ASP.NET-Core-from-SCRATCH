39e75b69f7a60ceeb326fd6c46b74737a6d874df
Zokni1Coder <erikkovacs0000@gmail.com>
2026-04-02 23:44:24 +0200

    StockApp with external RESTApi provider:MVC,Service,Options Pattern, Environment Variable Config, DIP.

--- ConfigurationExamples/StockApp/Controllers/HomeController.cs
+++ ConfigurationExamples/StockApp/Controllers/HomeController.cs
@@ -1,13 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using StockApp.Models;
+using StockApp.OptionsPatterns;
+using StockApp.ServiceContracts;
 
 namespace StockApp.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("/")]
-        public IActionResult Index()
+        private readonly IStockApiService _stockApiService;
+        private readonly IOptions<StockApiOptions> _options;
+
+        public HomeController(IStockApiService stockApiService, IOptions<StockApiOptions> options)
         {
-            return View();
+            this._stockApiService = stockApiService;
+            this._options = options;
+        }
+
+        [Route("/")]
+        public async Task<IActionResult> Index()
+        {
+            Dictionary<string, object>? GetStockResult = await
+                this._stockApiService.GetStocks(this._options.Value.quoteSymbol);
+
+            Stock stock = new Stock()
+            {
+                StockSymbol = this._options.Value.quoteSymbol,
+                CurrentPrice = Convert.ToDouble(GetStockResult["c"].ToString()),
+                HighPriceOfDay = Convert.ToDouble(GetStockResult["h"].ToString()),
+                LowPriceOfDay = Convert.ToDouble(GetStockResult["l"].ToString()),
+                OpenPrice = Convert.ToDouble(GetStockResult["o"].ToString())
+            };
+            return View(stock);
         }
     }
 }
--- ConfigurationExamples/StockApp/Program.cs
+++ ConfigurationExamples/StockApp/Program.cs
@@ -1,7 +1,13 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using StockApp.OptionsPatterns;
+using StockApp.ServiceContracts;
+using StockApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 //Regisztráljuk a HttpClient infrastruktúrát (IHttpClientFactory) a DI containerben
 builder.Services.AddHttpClient();
+builder.Services.AddScoped<IStockApiService, StockApiService>();
+builder.Services.Configure<StockApiOptions>(builder.Configuration.GetSection("StockAPI"));
 var app = builder.Build();
 
 app.UseStaticFiles();
