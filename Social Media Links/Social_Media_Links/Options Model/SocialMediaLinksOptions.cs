c4a8424a25e5e2fd1344a59b9f2e637421c9f621
Zokni1Coder <erikkovacs0000@gmail.com>
2026-04-03 21:29:04 +0200

    Social Media Links with Configuration

---
+++ Social Media Links/Social_Media_Links/Options Model/SocialMediaLinksOptions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Social_Media_Links.Options_Model
+{
+    public class SocialMediaLinksOptions
+    {
+        public string? Facebook { get; set; }
+        public string? Twitter { get; set; }
+        public string? Youtube { get; set; }
+        public string? Instagram { get; set; }
+    }
+}
