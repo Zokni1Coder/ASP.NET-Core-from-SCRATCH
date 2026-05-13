7dd31c57d4719dce93aaaf635de3f055459db98b
Zokni1Coder <erikkovacs0000@gmail.com>
2026-05-13 21:50:02 +0200

    Setting up the database.

--- CRUDWithXUnitExample/Entities/Country.cs
+++ CRUDWithXUnitExample/Entities/Country.cs
@@ -1,11 +1,15 @@
-﻿namespace Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
 {
     /// <summary>
     /// Ez maga a Country objektum, nem DTO, azaz Entity vagy Domain.
     /// </summary>
     public class Country
     {
+        [Key]
         public Guid Guid { get; set; }
+        [StringLength(40)]
         public string? Name { get; set; }
     }
 }
--- CRUDWithXUnitExample/Entities/Person.cs
+++ CRUDWithXUnitExample/Entities/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,16 @@ namespace Entities
     /// </summary>
     public class Person
     {
+        [Key] //Ezekkel az attribútumokkal tudjuk a db tábla megszoríásait definiáni.
         public Guid PersonId { get; set; }
+        [StringLength(40)] //érdemes megadni a maximumát egy nvarchar-nak, mert ha nem akkor úgy generáódik hogy nvarchar(max), ami nagyon leterhelné a db-t egy idő után. 
         public string? PersonName { get; set; }
         public string? Email { get; set; }
         public DateTime? DateOfBirth { get; set; }
+        [StringLength(10)]
         public string? Gender { get; set; }
         public Guid? CountryId { get; set; }
+        [StringLength(40)]
         public string? Address { get; set; }
         public bool ReceiveNewsLetter { get; set; }
     }
